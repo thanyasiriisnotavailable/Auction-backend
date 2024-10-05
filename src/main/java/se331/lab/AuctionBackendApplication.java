9ae20101d1c3d1693869690900be50c1c1d4dc5b
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-10-06 03:03:48 +0700

    Initial commit

---
+++ src/main/java/se331/lab/AuctionBackendApplication.java
@@ -0,0 +1,13 @@
+package se331.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se331/lab/AuctionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se331.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
